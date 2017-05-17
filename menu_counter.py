f028c5d38c8050d51de7d4bf5b4ea89b5c61c9e3
IvanPMakarenko <ivan.p.makarenko@gmail.com>
2017-05-17 22:07:10 +0700

    Homework 2.1
    Задание 2.1

---
+++ menu_counter.py
@@ -0,0 +1,68 @@
+with open('list_of_dishes.txt') as f:
+	# print(f.read())
+	cook_book = {}
+	for line in f:
+		dish = line.strip().lower()
+		#print (dish)
+		count_ingridients = int(f.readline().strip())
+		#print (count_ingridients)
+		i = 0
+		ingridients_list = []
+		while i < count_ingridients:
+			i += 1
+			ingridient = f.readline().lower().split('|')
+			ingridients = {'ingridient_name': None, 'quantity': None, 'measure': None}
+			ingridients['ingridient_name'] = ingridient[0].strip()
+			ingridients['quantity'] = int(ingridient[1].strip())
+			ingridients['measure'] = ingridient[2].strip()
+			#	{'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'}
+			ingridients_list.append(ingridients)
+		cook_book[dish] = ingridients_list
+		#print (ingridients_list)
+		f.readline()
+
+#print (cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+      shop_list = {}
+      for dish in dishes:
+        for ingridient in cook_book[dish]:
+          new_shop_list_item = dict(ingridient)
+
+          new_shop_list_item['quantity'] *= person_count
+          if new_shop_list_item['ingridient_name'] not in shop_list:
+            shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+          else:
+            shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+      return shop_list
+
+def print_shop_list(shop_list):
+  for shop_list_item in shop_list.values():
+    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],     shop_list_item['measure']))
+
+def check_dish_in_cook_book(dishes):
+	real_dishes = []
+	for dish in dishes:
+		if dish not in cook_book.keys():
+			continue
+		else:
+			real_dishes.append(dish)
+	return real_dishes #добавил новую функцию
+
+def create_shop_list():
+  person_count = int(input('Введите количество человек: '))
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+    .lower().split(', ')
+  real_dishes = check_dish_in_cook_book(dishes)
+  if len(real_dishes) == 0:
+  	print('Этих блюд в меню нет')
+  elif len(real_dishes) < len(dishes):
+  	print('В меню только:', *real_dishes)
+  else:
+  	shop_list = get_shop_list_by_dishes(real_dishes, person_count)
+  	print_shop_list(shop_list)
+
+create_shop_list()
+
+
+#print(check_dish_in_cook_book())
